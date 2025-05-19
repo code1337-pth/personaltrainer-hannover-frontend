d8aaac0e0551130108727717f3f3f02fa4e1e8a2
julian <julian.buehring@gmail.com>
2025-05-20 01:02:40 +0200

    seo verbessert

--- src/app/api/cache/refresh/route.ts
+++ src/app/api/cache/refresh/route.ts
@@ -4,6 +4,8 @@ import strapiCache from "@/lib/strapiCache";
 const SECRET = process.env.CACHE_REFRESH_SECRET;
 
 export async function POST(req: NextRequest) {
+    console.log("Header:", JSON.stringify(req.headers.get("authorization")));
+    console.log("Secret:", JSON.stringify(`Bearer ${SECRET}`));
     if (req.headers.get("authorization") !== `Bearer ${SECRET}`) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
