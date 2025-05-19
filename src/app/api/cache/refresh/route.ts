36071cf7179d373e5728c8a1f19b0d49d2ee2607
julian <julian.buehring@gmail.com>
2025-05-20 01:07:45 +0200

    cache api fix

--- src/app/api/cache/refresh/route.ts
+++ src/app/api/cache/refresh/route.ts
@@ -1,16 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import strapiCache from "@/lib/strapiCache";
 
 const SECRET = process.env.CACHE_REFRESH_SECRET;
 
 export async function POST(req: NextRequest) {
-    console.log("Header:", JSON.stringify(req.headers.get("authorization")));
-    console.log("Secret:", JSON.stringify(`Bearer ${SECRET}`));
     if (req.headers.get("authorization") !== `Bearer ${SECRET}`) {
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
     }
 
     strapiCache.clearCache()
     await strapiCache.preload();
-    return NextResponse.json({ success: true });
+    return NextResponse.json({success: true});
 }
