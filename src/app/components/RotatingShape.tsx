88ef6fb468b6e3c4c20db987e735aba3cd7fb58f
julian <julian.buehring@gmail.com>
2025-05-20 01:41:38 +0200

    seo performance

--- src/app/components/RotatingShape.tsx
+++ src/app/components/RotatingShape.tsx
@@ -6,7 +6,7 @@ const RotatingShape = () => {
         <section className="relative flex items-center justify-center">
             <div className="relative w-80 h-80 sm:w-100 sm:h-100 lg:w-[500px] lg:h-[500px] mt-20 sm:mt-0">
                 <Image
-                    src="/hero-profile-shape.avif"
+                    src="/hero-profile-shape.webp"
                     alt="Rotierendes Shape"
                     fill
                     sizes="(max-width: 661px) 80vw, 500px"
